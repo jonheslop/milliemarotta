b666ae97246430be69a4c2cbc6b6fca11aa6143a
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-08-27 18:45:51 +0100

    Better label

--- new/wp-content/themes/millie/archive-colouring_submission.php
+++ new/wp-content/themes/millie/archive-colouring_submission.php
@@ -5,7 +5,7 @@
 if ( is_day() ) { printf( __( 'Daily Archives: %s', 'blankslate' ), get_the_time( get_option( 'date_format' ) ) ); }
 elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'blankslate' ), get_the_time( 'F Y' ) ); }
 elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'blankslate' ), get_the_time( 'Y' ) ); }
-else { _e( 'Colouring', 'blankslate' ); }
+else { _e( 'Colouring Gallery', 'blankslate' ); }
 ?></h1>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
