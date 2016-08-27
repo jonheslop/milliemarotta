97feaf46efb8fc4fa1e3d8fac15d2c582edd3b37
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-08-27 23:34:30 +0100

    special page

---
+++ new/wp-content/themes/millie/page-creaturecuriosities.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<section id="content" role="main" class="container content-container">
+  <header class="header all-work-header">
+    <h1 class="entry-title">
+      Creature Curiosities
+    </h1>
+  </header>
+<?php $creatureArgs = array(
+    'post_type' => array('millie_creature'),
+    'posts_per_page' => -1,
+  );
+  $allWork = new WP_Query( $creatureArgs ); ?>
+<?php if ( $creature->have_posts() ) : while ( $creature->have_posts() ) : $creature->the_post(); ?>
+<?php get_template_part( 'grid' ); ?>
+<?php endwhile; endif; ?>
+<?php get_template_part( 'nav', 'below' ); ?>
+</section>
+<?php get_footer(); ?>
