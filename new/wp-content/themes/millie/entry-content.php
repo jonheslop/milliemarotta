4e56b4c584680279f0ba27691f62704b0827dc6b
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-09-06 21:13:54 +0100

    fixed if statment and p specificity

--- new/wp-content/themes/millie/entry-content.php
+++ new/wp-content/themes/millie/entry-content.php
@@ -1,7 +1,9 @@
 <section class="entry-content wrapper cf">
+  <?php if ( has_post_thumbnail() && get_post_type() != 'millie_work' ) : ?>
   <figure class="wrapper creature-crop">
-    <?php if ( has_post_thumbnail() && get_post_type() != 'millie_work' ) { the_post_thumbnail('large'); } ?>
+     <?php the_post_thumbnail('large'); ?>
   </figure>
+  <? endif; ?>
   <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
