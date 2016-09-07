ab8bee1c443406d6536d631bd2adaf988618c260
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-09-07 21:08:49 +0100

    added proper breadcrumbs

--- new/wp-content/themes/millie/single-colouring_submission.php
+++ new/wp-content/themes/millie/single-colouring_submission.php
@@ -2,11 +2,16 @@
 <section id="content" role="main" class="container content-container">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?>>
+      <span style="float:left;">&laquo;&nbsp;<a href="<?php echo get_post_type_archive_link('colouring_submission'); ?>">Back to submissions</a></span>
+      <?php $categories = get_the_terms($post->ID,'colouring_book');
+      $category_id = $categories[0]->term_id; 
+      ?>
+      <span style="float:right;"><a href="<?php echo get_term_link($category_id,'colouring_book'); ?>">View all in <?php echo $categories[0]->name; ?></a>&nbsp;&raquo;</span>
     <header class="wrapper work-header">
-      <h1 class="entry-title">
+        <h1 class="entry-title">
         <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">by <?php the_field('name'); ?></a>
       </h1>
-    </header>
+  </header>
     <section class="entry-content wrapper">
       <?php $materials = get_field('materials');
         if ( !empty($materials) ) : ?>
@@ -20,8 +25,6 @@
             <p><strong><em>Millie says:</em></strong> <?php echo $millies_comments; ?></p>
           </div>
         <? endif; ?>
-      <span style="float:left;">&laquo;&nbsp;<a href="<?php echo get_post_type_archive_link('colouring_submission'); ?>">Back to submissions</a></span>
-      <span style="float:right;"><a href="<?php echo get_post_type_archive_link('colouring_submission'); ?>">View all </a>&nbsp;&raquo;</span>
     </section>
       <?php $image = get_field('image');
           if ( !empty($image) ) {
