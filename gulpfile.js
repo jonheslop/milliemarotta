43363bd1886f5a4deb2c806aa55084860aefaf99
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-09-18 11:04:30 +0100

    Fixed float layout

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 
 // Compile sass, minify css, autoprefix
 gulp.task('styles', function() {
-  return gulp.src('./new/wp-content/themes/millie/css/sass/**/*.scss')
+  return gulp.src('./wp-content/themes/millie/css/sass/**/*.scss')
     .pipe(sass({
       includePaths: require('node-bourbon').includePaths
     }))
@@ -16,13 +16,13 @@ gulp.task('styles', function() {
       console.log(err.message);
     })
     .pipe(nano())
-    .pipe(gulp.dest('./new/wp-content/themes/millie/'))
+    .pipe(gulp.dest('./wp-content/themes/millie/'))
     .pipe(livereload());
 });
 
 // // ESLint
 gulp.task('eslint', function() {
-  return gulp.src('./new/wp-content/themes/millie/js/parts/**/*.js')
+  return gulp.src('./wp-content/themes/millie/js/parts/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
@@ -30,21 +30,21 @@ gulp.task('eslint', function() {
 
 // // Minify js files
 gulp.task('scripts', ['eslint'], function() {
-  return gulp.src('./new/wp-content/themes/millie/js/parts/**/*.js')
+  return gulp.src('./wp-content/themes/millie/js/parts/**/*.js')
     .pipe(concat('site.min.js'))
     .pipe(uglify())
     .on('error', function(err) {
       console.log(err.message);
     })
-    .pipe(gulp.dest('./new/wp-content/themes/millie/js'))
+    .pipe(gulp.dest('./wp-content/themes/millie/js'))
     .pipe(livereload());
 });
 
 // Watch assets for changes and lunch relevant task
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('new/wp-content/themes/millie/css/sass/**/*.scss', ['styles']);
-  gulp.watch('new/wp-content/themes/millie/js/**/*.js', ['scripts']);
+  gulp.watch('wp-content/themes/millie/css/sass/**/*.scss', ['styles']);
+  gulp.watch('wp-content/themes/millie/js/**/*.js', ['scripts']);
 
   // Watch HTML and livereload
   gulp.watch('*.{html,php}')
--- wp-content/themes/millie/taxonomy-colouring_book.php
+++ wp-content/themes/millie/taxonomy-colouring_book.php
@@ -2,35 +2,36 @@
 <section id="content" role="main" class="container content-container">
 <header class="header work-header">
 
-<h1><?php $currentTaxTerm = get_queried_object()->term_id; ?></h1>
-<h1 class="entry-title">Welcome to the Colouring Gallery</h1>
-<section class="entry-content">
-  <p>Share your work here and browse all the wonderful submissions from others. Whether you&rsquo;re after colour palette inspiration, ideas for interesting techniques or just want to have a gander, peruse at your&nbsp;leisure.</p>
-</section>
+  <h1><?php $currentTaxTerm = get_queried_object()->term_id; ?></h1>
+  <h1 class="entry-title">Welcome to the Colouring Gallery</h1>
+  <section class="entry-content">
+    <p>Share your work here and browse all the wonderful submissions from others. Whether you&rsquo;re after colour palette inspiration, ideas for interesting techniques or just want to have a gander, peruse at your&nbsp;leisure.</p>
+  </section>
 
-<?php $categories = get_terms( 'colouring_book', array(
-    'orderby' => 'slug',
-    'order' => 'ASC',
-    'hide_empty' => 0
-) ); ?>
+  <?php $categories = get_terms( 'colouring_book', array(
+      'orderby' => 'slug',
+      'order' => 'ASC',
+      'hide_empty' => 0
+  ) ); ?>
 
-<p class="colouring-submit wrapper cf"><strong>Filter by colouring book:</strong>
-  <a href="<?php echo get_post_type_archive_link('colouring_submission'); ?>">All</a>,
-  <?php $index = 1;
-  $categoriesCount = count($categories);
-  foreach ($categories as $category ) : ?>
-    <?php if ($currentTaxTerm == $category->term_id) : ?>
-    <a class="current" href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
-      echo ', ';
-    } ?>
-    <?php else : ?>
-    <a href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
-      echo ', ';
-    } ?>
-    <?php endif; ?>
-    <?php $index++; ?>
-  <?php endforeach; ?>
-  <a class="button button_buy colouring_button" href="<?php echo get_permalink(27); ?>">Submit your colouring here</a></p>
+  <p class="colouring-submit wrapper cf"><strong>Filter by colouring book:</strong>
+    <a href="<?php echo get_post_type_archive_link('colouring_submission'); ?>">All</a>,
+    <?php $index = 1;
+    $categoriesCount = count($categories);
+    foreach ($categories as $category ) : ?>
+      <?php if ($currentTaxTerm == $category->term_id) : ?>
+      <a class="current" href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
+        echo ', ';
+      } ?>
+      <?php else : ?>
+      <a href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
+        echo ', ';
+      } ?>
+      <?php endif; ?>
+      <?php $index++; ?>
+    <?php endforeach; ?>
+  </p>
+<a class="button button_buy colouring_button" href="<?php echo get_permalink(27); ?>">Submit your colouring here</a>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'grid' ); ?>
