9476b697c3aadc3305bc604538b36ec1e4295941
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-09-18 11:12:59 +0100

    other page

--- wp-content/themes/millie/archive-colouring_submission.php
+++ wp-content/themes/millie/archive-colouring_submission.php
@@ -18,17 +18,20 @@ elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'blankslate' ), get_th
     'hide_empty' => 0
 ) ); ?>
 
-<p class="colouring-submit wrapper cf"><strong>Filter by colouring book:</strong>
-    <a href="/" class="current">All</a>,
-  <?php $index = 1;
-  $categoriesCount = count($categories);
-  foreach ($categories as $category ) : ?>
-    <a href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
-      echo ', ';
-    } ?>
-    <?php $index++; ?>
-  <?php endforeach; ?>
-  <a class="button button_buy colouring_button" href="<?php echo get_permalink(27); ?>">Submit your colouring here</a></p>
+  <div class="cf">
+    <p class="colouring-submit wrapper cf"><strong>Filter by colouring book:</strong>
+        <a href="/" class="current">All</a>,
+      <?php $index = 1;
+      $categoriesCount = count($categories);
+      foreach ($categories as $category ) : ?>
+        <a href="<?php echo get_term_link($category->slug, 'colouring_book') ?>"><?php echo $category->name; ?></a><?php if ($categoriesCount != $index) {
+          echo ', ';
+        } ?>
+        <?php $index++; ?>
+      <?php endforeach; ?>
+    </p>
+    <a class="button button_buy colouring_button" href="<?php echo get_permalink(27); ?>">Submit your colouring here</a></p>
+  </div>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'grid' ); ?>
