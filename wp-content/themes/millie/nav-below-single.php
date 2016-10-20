ac57add8bc6423ac92b9792b0f4b918656b37255
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-10-20 10:15:51 +0100

    switch single arrows

--- wp-content/themes/millie/nav-below-single.php
+++ wp-content/themes/millie/nav-below-single.php
@@ -1,4 +1,4 @@
 <nav id="nav-below" class="navigation pagination" role="navigation">
-<div class="nav-previous wrapper"><?php previous_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
-<div class="nav-next wrapper"><?php next_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
+<div class="nav-previous wrapper"><?php next_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
+<div class="nav-next wrapper"><?php previous_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
 </nav>
--- wp-content/themes/millie/nav-below.php
+++ wp-content/themes/millie/nav-below.php
@@ -1,6 +1,6 @@
 <?php global $wp_query; if ( $wp_query->max_num_pages > 1 ) { ?>
 <nav id="nav-below" class="navigation pagination" role="navigation">
-<div class="nav-previous wrapper"><?php previous_posts_link(sprintf( __( 'newer %s', 'blankslate' ), '<span class="meta-nav">&larr;</span>' ) ) ?></div>
-<div class="nav-next wrapper"><?php next_posts_link(sprintf( __( '%s older', 'blankslate' ), '<span class="meta-nav">&rarr;</span>' ) ) ?></div>
+<div class="nav-previous wrapper"><?php next_posts_link(sprintf( __( '%s older', 'blankslate' ), '<span class="meta-nav">&larr;</span>' ) ) ?></div>
+<div class="nav-next wrapper"><?php previous_posts_link(sprintf( __( 'newer %s', 'blankslate' ), '<span class="meta-nav">&rarr;</span>' ) ) ?></div>
 </nav>
 <?php } ?>
