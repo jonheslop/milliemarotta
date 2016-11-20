7870780801b91b6be0595c707cb9198fbd4f09ab
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2016-11-20 12:28:24 +0000

    FB SDK

--- wp-content/themes/millie/footer.php
+++ wp-content/themes/millie/footer.php
@@ -35,5 +35,22 @@
     ga('create', 'UA-83255347-1', 'auto');
     ga('send', 'pageview');
   </script>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1247312838662572',
+        xfbml      : true,
+        version    : 'v2.8'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  </script>
 </body>
 </html>
