a5bb18fe464f4b81256b104947c31aa6cc2db8b2
Jon Heslop <jonheslop@Jons-MacBook-Air.local>
2017-01-07 13:48:20 +0000

    removed from next/prev as doesnt work

--- wp-content/themes/millie/nav-below-single.php
+++ wp-content/themes/millie/nav-below-single.php
@@ -1,13 +1,4 @@
-<? $pageIndex = $_GET['pageIndex']; ?>
 <nav id="nav-below" class="navigation pagination" role="navigation">
-<? if ($pageIndex) : ?>
-  <div class="nav-previous wrapper"><?php next_post_link( '%link' . ?pageIndex=<?= $pageIndex; ?>, '<span class="meta-nav">&larr;</span> %title' ); ?></div>
- <? else : ?>
-  <div class="nav-previous wrapper"><?php next_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
- <? endif; ?>
- <? if ($pageIndex) : ?>
- <div class="nav-next wrapper"><?php previous_post_link( '%link' . ?pageIndex=<?= $pageIndex; ?>, '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
-<? else : ?>
- <div class="nav-next wrapper"><?php previous_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
-<? endif; ?>
+<div class="nav-previous wrapper"><?php next_post_link( '%link', '<span class="meta-nav">&larr;</span> %title' ); ?></div>
+<div class="nav-next wrapper"><?php previous_post_link( '%link', '%title <span class="meta-nav">&rarr;</span>' ); ?></div>
 </nav>
