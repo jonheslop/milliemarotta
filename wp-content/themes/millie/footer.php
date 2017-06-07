bcac9cac625100d7cc9459228ff15ea71758ccc8
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-06-07 16:58:11 +0100

    class not ID

--- wp-content/themes/millie/footer.php
+++ wp-content/themes/millie/footer.php
@@ -2,7 +2,7 @@
     <div class="cf app-button-wrap">
         <a href="https://itunes.apple.com/us/app/id1064432319" title="Download Millie Marotta's Coloring Adventures"><img class="app-button smaller" src="<?php echo get_template_directory_uri(); ?>/img/app-store-badge.svg" alt="Millie Marotta's Coloring Adventures"></a>
     </div>
-    <div id="copyright">
+    <div class="copyright">
   		<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
   	</div>
     <aside class="social-icons">
