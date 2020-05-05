0999dcc3a3984ca84adc27202b8d9708829df5af
Jon Heslop <jon@jonheslop.com>
2020-05-05 09:17:14 +0100

    Use div

--- wp-content/themes/millie/category-downloads.php
+++ wp-content/themes/millie/category-downloads.php
@@ -2,7 +2,7 @@
 <section id="content" role="main" class="container content-container">
 <header class="header all-work-header downloads">
   <h1 class="entry-title"><?php single_cat_title(); ?></h1>
-  <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<p class="entry-content">' . category_description() . '</p>' ); ?>
+  <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'grid' ); ?>
