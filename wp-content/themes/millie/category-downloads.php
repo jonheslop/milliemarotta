64900cce509b610c2d47fa4471fa10deed2fc01a
Jon Heslop <jon@jonheslop.com>
2020-05-05 09:11:48 +0100

    Add custom archive page for downloads

--- wp-content/themes/millie/category-downloads.php
+++ wp-content/themes/millie/category-downloads.php
@@ -3,6 +3,9 @@
 <header class="header all-work-header">
   <h1 class="entry-title"><?php single_cat_title(); ?></h1>
   <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<p class="archive-meta">' . category_description() . '</p>' ); ?>
+  <?php 
+    the_archive_description( '<p class="taxonomy-description">', '</p>' ); 
+  ?>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'grid' ); ?>
