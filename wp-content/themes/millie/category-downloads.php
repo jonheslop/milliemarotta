985364f79061441e88e8845f36171b8c0cd7742d
Jon Heslop <jon@jonheslop.com>
2020-05-05 09:16:21 +0100

    Add styles

--- wp-content/themes/millie/category-downloads.php
+++ wp-content/themes/millie/category-downloads.php
@@ -2,10 +2,7 @@
 <section id="content" role="main" class="container content-container">
 <header class="header all-work-header downloads">
   <h1 class="entry-title"><?php single_cat_title(); ?></h1>
-  <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<p class="archive-meta">' . category_description() . '</p>' ); ?>
-  <?php 
-    the_archive_description( '<p class="taxonomy-description">', '</p>' ); 
-  ?>
+  <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<p class="entry-content">' . category_description() . '</p>' ); ?>
 </header>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'grid' ); ?>
