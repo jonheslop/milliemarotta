f7fddd4d153bd3717d1f910a24567520d49838b5
Jon Heslop <jon@jonheslop.com>
2020-05-06 21:45:33 +0100

    Getting somewhere but code in right file

--- wp-content/themes/millie/page-downloads.php
+++ wp-content/themes/millie/page-downloads.php
@@ -11,9 +11,13 @@
     <header class="header all-work-header">
       <h1 class="entry-title">More <?php echo $current_post_category[0]->name; ?></h1>
     </header>
-      <?php while ( $allDownloads->have_posts() ) : $allDownloads->the_post(); ?>
-        <?php get_template_part( 'grid' ); ?>
-  <?php endwhile; endif; ?>
+    <?php while ( $allDownloads->have_posts() ) : $allDownloads->the_post(); ?>
+      <?php if ( wp_attachment_is_image( $post->ID ) ) : $att_image = wp_get_attachment_image_src( $post->ID, "large" ); ?>
+        <p class="attachment"><a href="<?php echo wp_get_attachment_url( $post->ID ); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image[0]; ?>" width="<?php echo $att_image[1]; ?>" height="<?php echo $att_image[2]; ?>" class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a></p>
+      <?php else : ?>
+        <a href="<?php echo wp_get_attachment_url( $post->ID ); ?>" title="<?php echo esc_html( get_the_title( $post->ID ), 1 ); ?>" rel="attachment"><?php echo basename( $post->guid ); ?></a>
+      <?php endif; ?>
+    <?php endwhile; endif; ?>
 
 <?php // get_template_part( 'grid' ); ?>
 <?php // get_template_part( 'nav', 'below' ); ?>
