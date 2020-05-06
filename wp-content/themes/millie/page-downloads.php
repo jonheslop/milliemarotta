1db85c9bf1d9d1070f22dad1c1316a369b746a7f
Jon Heslop <jon@jonheslop.com>
2020-05-06 21:55:07 +0100

    try this

--- wp-content/themes/millie/page-downloads.php
+++ wp-content/themes/millie/page-downloads.php
@@ -4,7 +4,6 @@
 <?php $allDownloadsArgs = array(
     'post_type' => 'attachment',
     'posts_per_page' => -1,
-    'cat' => 20,
   );
   $allDownloads = new WP_Query( $allDownloadsArgs ); 
   if ( $allDownloads->have_posts() ) : ?>
@@ -12,10 +11,10 @@
       <h1 class="entry-title">More <?php echo $current_post_category[0]->name; ?></h1>
     </header>
     <?php while ( $allDownloads->have_posts() ) : $allDownloads->the_post(); ?>
-      <?php if ( wp_attachment_is_image( $allWork->ID ) ) : $att_image = wp_get_attachment_image_src( $allWork->ID, "large" ); ?>
-        <p class="attachment"><a href="<?php echo wp_get_attachment_url( $allWork->ID ); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image[0]; ?>" width="<?php echo $att_image[1]; ?>" height="<?php echo $att_image[2]; ?>" class="attachment-medium" alt="<?php $allWork->post_excerpt; ?>" /></a></p>
+      <?php if ( wp_attachment_is_image( $post->ID ) ) : $att_image = wp_get_attachment_image_src( $post->ID, "large" ); ?>
+        <p class="attachment"><a href="<?php echo wp_get_attachment_url( $post->ID ); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image[0]; ?>" width="<?php echo $att_image[1]; ?>" height="<?php echo $att_image[2]; ?>" class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a></p>
       <?php else : ?>
-        <a href="<?php echo wp_get_attachment_url( $allWork->ID ); ?>" title="<?php echo esc_html( get_the_title( $allWork->ID ), 1 ); ?>" rel="attachment"><?php echo basename( $allWork->guid ); ?></a>
+        <a href="<?php echo wp_get_attachment_url( $post->ID ); ?>" title="<?php echo esc_html( get_the_title( $post->ID ), 1 ); ?>" rel="attachment"><?php echo basename( $post->guid ); ?></a>
       <?php endif; ?>
     <?php endwhile; ?>
     <?php else : ?>
