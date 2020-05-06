a81ab1c9e25382f4fa7fbe56f6c2660e3f6be19e
Jon Heslop <jon@jonheslop.com>
2020-05-06 22:23:47 +0100

    Use grid

--- wp-content/themes/millie/page-downloads.php
+++ wp-content/themes/millie/page-downloads.php
@@ -1,5 +1,55 @@
 <?php get_header(); ?>
 <link href="https://unpkg.com/tachyons@4.10.0/css/tachyons.css" rel="stylesheet">
+<style type="text/css">
+.g2x {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.g4x {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+.gg1 {
+  grid-gap: 1rem;
+}
+.gg2 {
+  grid-gap: 2rem;
+}
+
+@media screen and (min-width: 40em) and (max-width: 60em) {
+  .g2x-m {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .g4x-m {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  .gg1-m {
+    grid-gap: 1rem;
+  }
+  .gg2-m {
+    grid-gap: 2rem;
+  }
+}
+
+@media screen and (min-width: 60em) {
+  .g2x-l {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  .g4x-l {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  .gg1-l {
+    grid-gap: 1rem;
+  }
+  .gg2-l {
+    grid-gap: 2rem;
+  }
+}
+</style>
 
 <section id="content" role="main" class="container content-container">
 
@@ -15,9 +65,9 @@
     <header class="header all-work-header">
       <h1 class="entry-title">Downloads</h1>
     </header>
-    <div class="cf">
+    <div class="g2x g4x-l">
     <?php while ( $allDownloads->have_posts() ) : $allDownloads->the_post(); ?>
-      <div class="fl w-100 w-50-m w-25-l">
+      <div class="">
         <img class="w-100 db mb4" src="<?php echo wp_get_attachment_image_src( $post->ID, "grande" )[0] ?>" alt="<?php the_title() ?> -<?php $post->post_excerpt; ?>" />
         <h4><?php the_title() ?></h4>
       </div>
