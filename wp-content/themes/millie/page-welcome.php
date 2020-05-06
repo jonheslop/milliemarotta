987e2b96c66fa5c6902948dc25e89cc294d4658a
Jon Heslop <jon@jonheslop.com>
2020-05-06 22:56:33 +0100

    Styling

--- wp-content/themes/millie/page-welcome.php
+++ wp-content/themes/millie/page-welcome.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
-<link href="https://unpkg.com/tachyons@4.10.0/css/tachyons.css" rel="stylesheet">
 <section id="content" role="main" class="container content-container">
+<style type="text/css">
+  .entry-content {
+    text-align: center;
+  }
+
+.entry-content a {
+    display: block;
+    padding: 1rem;
+    border: 1px dashed;
+  }
+</style>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -8,13 +18,13 @@
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header>
   <?php if ( has_post_thumbnail() ) : ?>
-  <section class="entry-content cf">
-    <figure class="wrapper page-image">
-       <?php the_post_thumbnail('large'); ?>
-    </figure>
-  </section>
+    <section class="entry-content cf">
+      <figure class="wrapper page-image">
+        <?php the_post_thumbnail('large'); ?>
+      </figure>
+    </section>
+  <? endif; ?>
 	<section class="entry-content">
-    <? endif; ?>
 		<?php the_content(); ?>
 	</section>
 </article>
