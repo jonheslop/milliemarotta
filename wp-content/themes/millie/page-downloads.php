e7f7461b10d46c975d9f8c4c727d559826a65de5
Jon Heslop <jon@jonheslop.com>
2020-05-07 11:00:36 +0100

    Fix caption to bittom

--- wp-content/themes/millie/page-downloads.php
+++ wp-content/themes/millie/page-downloads.php
@@ -74,7 +74,7 @@
         <a class="db link grow" href="<?php echo wp_get_attachment_url( $post->ID ); ?>" rel="attachment">
           <img class="w-100 db mb4" src="<?php echo wp_get_attachment_image_src( $post->ID, "grande" )[0] ?>" alt="<?php the_title() ?> -<?php $post->post_excerpt; ?>" />
         </a>
-        <h4 class="tc pb3 ma0 bb b--silver" style="margin-bottom: auto;"><?php echo wp_get_attachment_caption() ?></h4>
+        <h4 class="tc pb3 ma0 bb b--silver" style="margin-top: auto;"><?php echo wp_get_attachment_caption() ?></h4>
       </div>
     <?php endwhile; ?>
     </div>
