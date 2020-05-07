444239898cef5a9dbe8e1d9b12340199bef49007
Jon Heslop <jon@jonheslop.com>
2020-05-07 17:39:10 +0100

    drop the

--- wp-content/themes/millie/archive.php
+++ wp-content/themes/millie/archive.php
@@ -91,7 +91,7 @@ else { _e( 'Archives', 'blankslate' ); }
           <?php the_post_thumbnail('grande', array('class' => 'w-100 h-auto db')) ?>
         <?php endif; ?>
       </a>
-      <h4 class="tc pb3 ma0 silver" style="margin-bottom: auto;"><? print_r(get_the_field('book')); ?></h4>
+      <h4 class="tc pb3 ma0 silver" style="margin-bottom: auto;"><? print_r(get_field('book')); ?></h4>
     </div>
   <?php endwhile; ?>
   </div>
